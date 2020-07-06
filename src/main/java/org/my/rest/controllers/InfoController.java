808c9c5312a99026d909a57a2694cbe6f12b1390
MariiaS9 <masher_f@mail.ru>
2020-07-06 16:52:37 +0200

    InfoController added

---
+++ src/main/java/org/my/rest/controllers/InfoController.java
@@ -0,0 +1,14 @@
+package org.my.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @GetMapping("/info")
+    public String getInfo(){
+        return "My Rest example";
+    }
+}
